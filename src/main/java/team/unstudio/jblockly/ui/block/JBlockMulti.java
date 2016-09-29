e801aa265ed78a8c2b1358c6cf5278f48aed3b8a
Mouse <937279156@163.com>
2016-09-29 11:56:12 +0800

    Maven

---
+++ src/main/java/team/unstudio/jblockly/ui/block/JBlockMulti.java
@@ -0,0 +1,8 @@
+package team.unstudio.jblockly.ui.block;
+
+public abstract class JBlockMulti extends JBlockBase{
+
+	private static final long serialVersionUID = 1L;
+	
+	public JBlockMulti() {}
+}
--- src/team/unstudio/jblockly/ui/block/JBlockMulti.java
+++
@@ -1,8 +0,0 @@
-package team.unstudio.jblockly.ui.block;
-
-public abstract class JBlockMulti extends JBlockBase{
-
-	private static final long serialVersionUID = 1L;
-	
-	public JBlockMulti() {}
-}
